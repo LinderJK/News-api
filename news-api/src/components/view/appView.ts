d1afbd039ea1af7cde0372d11f1a11ca704c0b9b
LinderJK <slepchenkovia@gmail.com>
2024-02-26 22:54:39 +0300

    feat: change appView.js to TS

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,18 +1,21 @@
-import News from './news/news';
-import Sources from './sources/sources';
+import News, { ArticlesArray } from './news/news';
+import Sources, { SourcesArray } from './sources/sources';
 
 export class AppView {
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: { articles?: ArticlesArray }): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: { sources?: SourcesArray }): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
