02ed4abe2bdedae661f1ab88666c5001ced8aef0
LinderJK <slepchenkovia@gmail.com>
2024-03-01 22:51:57 +0300

    fix: types in class News

--- news-api/src/components/view/news/news.ts
+++ news-api/src/components/view/news/news.ts
@@ -18,19 +18,19 @@ export type ArticlesArray = Array<Article>;
 
 class News {
     draw(data: ArticlesArray): void {
-        const news: Article[] = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+        const news: ArticlesArray = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
         const fragment: DocumentFragment = document.createDocumentFragment();
         const newsItemTemp: HTMLTemplateElement | null = document.querySelector('#newsItemTemp');
 
         news.forEach((item: Article, idx: number): void => {
             if (!newsItemTemp) return;
-            const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
+            const newsClone: HTMLElement | null = newsItemTemp.content.cloneNode(true) as HTMLElement;
 
             if (idx % 2) (newsClone.querySelector('.news__item') as HTMLElement).classList.add('alt');
 
             (newsClone.querySelector('.news__meta-photo') as HTMLElement).style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
+                item.urlToImage || 'assets/news_placeholder.jpg'
             })`;
             (newsClone.querySelector('.news__meta-author') as HTMLElement).textContent =
                 item.author || item.source.name;
